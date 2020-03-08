fa82e2debb7edf3eae102f6c9a6ce09c12b6cff3
Rakib Khan <rakibkhan@live.co.uk>
2020-03-08 01:58:43 +0000

    .

--- learningStuff/fileLearning.py
+++ learningStuff/fileLearning.py
@@ -1,12 +1,19 @@
-f = open('randomData.txt', 'r')
+# f = open('randomData.txt', 'r')
 
-# fileContents = f.readline()
-# fileContents2 = f.readline()
-# print(fileContents2)
+# # fileContents = f.readline()
+# # fileContents2 = f.readline()
+# # print(fileContents2)
 
-# for x in f:
-#     print(x)
+# # for x in f:
+# #     print(x)
 
-print(f.read(30))
+# # print(f.read(30))
 
+# f.write('hello')
+
+# f.close()
+
+f = open('text2.txt', 'w')
+f.write('howdy there stranger')
+f.write('what')
 f.close()
